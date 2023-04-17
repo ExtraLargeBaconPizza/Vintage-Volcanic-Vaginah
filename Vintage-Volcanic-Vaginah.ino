a1c79b9cd2a0dbffa007b50c3add42a6040c7d95
ExtraLargeBaconPizza <62860274+ExtraLargeBaconPizza@users.noreply.github.com>
2023-04-16 21:03:01 -0600

    flow too fast fixed
    lavaFlow function was using a static heat array. This meant a single heat array was being used for all strips

--- Vintage-Volcanic-Vaginah.ino
+++ Vintage-Volcanic-Vaginah.ino
@@ -4,9 +4,9 @@
 	Pinout Reference: https://esphome.io/devices/nodemcu_esp32.html
 
     Notes:
-    - Cannot use addLeds in loop because the pin number must be a runtime constant. Gross, I know.
-    - Ensure esp32 is ground to psu, even if powered by seperate usb
-    - ws2815 are single direction. you must connect to it with a male JST SM
+    - Cannot use loop in addLeds function because the pin number must be a runtime constant. Gross, I know.
+    - Ensure esp32 is grounded to psu, even if powered by seperate usb
+    - ws2815 are unidirectional. You must connect to the female end of the strip to a male JST SM it from the board
     - Parrallel output  - writing to each pixel takes 30µs, so FastLED.show() takes a very long time because it has to iterate over all pixels and strips in series (ie one at a time)
                         - parrallel will write to strips in parrallel and must be enabled
                         - reddit explination: https://www.reddit.com/r/FastLED/comments/aqlb94/troubleshooting_slow_performance_tied_to/
@@ -27,16 +27,20 @@
 #include <FastLED.h>
 
 // Enable parrallel output
-// #define FASTLED_ESP32_I2S true
-// #define FASTLED_RMT_MAX_CHANNELS 8
+#define FASTLED_ESP32_I2S true
+#define FASTLED_RMT_MAX_CHANNELS 8
 
 #define COLOR_ORDER RGB
 #define CHIPSET     WS2811
 #define NUM_LEDS    300
 #define NUM_STRIPS 	8
-#define FRAMES_PER_SECOND 30
+#define FRAMES_PER_SECOND 60
+
+#define COOLING 55
+#define SPARKLING 120
 
 CRGB leds[NUM_STRIPS][NUM_LEDS];
+byte heat[NUM_STRIPS][NUM_LEDS];
 
 void setup() 
 {
@@ -77,32 +81,29 @@ void loop()
 {
     for (int i = 0; i < 8; i++)
     {
-        lava(i, 55, 120, false);
+        lavaFlow(i, false);
     }
 
     FastLED.show();
     FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
 
-void lava(int stripIndex, int cooling, int sparking, bool Erupting) 
+void lavaFlow(int stripIndex, bool Erupting) 
 {
-	static byte heat[NUM_LEDS];
-	int cooldown;
-	
 	// Step 1.  Cool down every cell a little
 	if (!Erupting)
 	{
 		for (int i = 0; i < NUM_LEDS; i++) 
 		{
-			cooldown = random(0, ((cooling * 10) / NUM_LEDS) + 2);
+			int cooldown = random(0, ((COOLING * 10) / NUM_LEDS) + 2);
 			
-			if (cooldown > heat[i]) 
+			if (cooldown > heat[stripIndex][i]) 
 			{
-				heat[i] = 0;
+				heat[stripIndex][i] = 0;
 			} 
 			else 
 			{
-				heat[i] = heat[i] - cooldown;
+				heat[stripIndex][i] = heat[stripIndex][i] - cooldown;
 			}
 		}
 	}
@@ -110,20 +111,20 @@ void lava(int stripIndex, int cooling, int sparking, bool Erupting)
 	// Step 2.  Heat from each cell drifts 'down' and diffuses a little
 	for (int i = NUM_LEDS - 1; i >= 2; i--) 
 	{
-		heat[i] = (heat[i - 1] + heat[i - 2] + heat[i - 2]) / 3;
+		heat[stripIndex][i] = (heat[stripIndex][i - 1] + heat[stripIndex][i - 2] + heat[stripIndex][i - 2]) / 3;
 	}
 
 	// Step 3.  Randomly ignite new 'sparks' near the top
-	if (random(255) < sparking) 
+	if (random(255) < SPARKLING) 
 	{
 		int y = random(7);
 
-		heat[y] = heat[y] + random(160,255);
+		heat[stripIndex][y] = heat[stripIndex][y] + random(160,255);
 	}
 
 	// Step 4.  Convert heat to LED colors
 	for (int i = 0; i < NUM_LEDS; i++) 
 	{
-        leds[stripIndex][i] = HeatColor(heat[i]);
+        leds[stripIndex][i] = HeatColor(heat[stripIndex][i]);
 	}
 }
